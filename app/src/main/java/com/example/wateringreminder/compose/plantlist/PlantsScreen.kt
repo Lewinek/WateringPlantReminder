0cc4f3a85950a5f53553fede273f6f0e564faf6a
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-03-29 19:47:57 +0200

    Add view to PlantsScreen

--- app/src/main/java/com/example/wateringreminder/compose/plantlist/PlantsScreen.kt
+++ app/src/main/java/com/example/wateringreminder/compose/plantlist/PlantsScreen.kt
@@ -1,8 +1,37 @@
 package com.example.wateringreminder.compose.plantlist
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.wateringreminder.compose.articlelist.ArticlesScreen
 
 @Composable
 fun PlantsScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Magenta),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Plants",
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}
 
+@Preview
+@Composable
+fun PlantsScreenPreview() {
+    PlantsScreen()
 }
