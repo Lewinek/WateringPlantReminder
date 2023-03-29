f801c4d5c8a4f0bd232eda17aae77ffcb40cb51d
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-03-29 17:40:30 +0200

    Add MainScreen

---
+++ app/src/main/java/com/example/wateringreminder/MainScreen.kt
@@ -0,0 +1,15 @@
+package com.example.wateringreminder
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {}
+    ) {
+
+    }
+}
