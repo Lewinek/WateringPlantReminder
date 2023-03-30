6b1f307eebc8fb089e25f881fc3ffc5c3deb0b43
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-03-30 16:39:22 +0200

    Add PlantCached constructor and method to help map PlantCached to Plant

--- data_source/src/main/java/com/example/data_source/local/PlantCached.kt
+++ data_source/src/main/java/com/example/data_source/local/PlantCached.kt
@@ -2,9 +2,20 @@ package com.example.data_source.local
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.data_source.Plant
 
 @Entity
 data class PlantCached(
     @PrimaryKey val id: Int? = null,
     val name: String,
-)
+) {
+    constructor(plant: Plant) : this(
+        plant.id,
+        plant.name,
+    )
+
+    fun toPlant() = Plant(
+        id = id,
+        name = name
+    )
+}
