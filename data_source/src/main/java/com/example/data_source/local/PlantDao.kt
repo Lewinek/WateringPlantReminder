b4c6190c232674a0568a41b85245e7e25262c892
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-03-30 12:53:26 +0200

    Add insert plant function into PlantDao

--- data_source/src/main/java/com/example/data_source/local/PlantDao.kt
+++ data_source/src/main/java/com/example/data_source/local/PlantDao.kt
@@ -1,6 +1,8 @@
 package com.example.data_source.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
@@ -9,4 +11,7 @@ interface PlantDao {
 
     @Query("SELECT * FROM plantCached")
     fun getPlants(): Flow<List<PlantCached>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPlant(plantCached: PlantCached)
 }
