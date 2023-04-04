5da67750831e6465611d009a96d3cfaf5d1bde6e
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-04-04 12:13:36 +0200

    Change file location

--- app/src/main/java/com/example/wateringreminder/plantcreator/PlantCreationScreen.kt
+++ app/src/main/java/com/example/wateringreminder/plantcreator/PlantCreationScreen.kt
@@ -1,4 +1,4 @@
-package com.example.wateringreminder.compose
+package com.example.wateringreminder.plantcreator
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.wateringreminder.plantcreator.PlantCreationViewModel
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
