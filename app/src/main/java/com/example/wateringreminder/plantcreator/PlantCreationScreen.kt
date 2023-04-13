41a83140dee882915eb7b3505a457fd9dd447139
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-04-13 20:40:24 +0200

    Add water days buttons in LazyRow

--- app/src/main/java/com/example/wateringreminder/plantcreator/PlantCreationScreen.kt
+++ app/src/main/java/com/example/wateringreminder/plantcreator/PlantCreationScreen.kt
@@ -2,18 +2,23 @@ package com.example.wateringreminder.plantcreator
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.wateringreminder.AddButton
+import com.example.wateringreminder.DayButton
 import com.example.wateringreminder.LabelTextField
 import com.example.wateringreminder.R
+import com.example.wateringreminder.ui.theme.DarkText
 import com.example.wateringreminder.ui.theme.LightBlue
 import org.koin.androidx.compose.koinViewModel
 
@@ -53,6 +58,28 @@ fun PlantCreationScreen() {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 LabelTextField("Name", onValueChange = { viewModel.updatePlantName(it) })
                 LabelTextField("Location", onValueChange = { viewModel.updatePlantLocation(it) })
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Start)
+                        .padding(top = 8.dp)
+                ) {
+                    Text(
+                        text = "Water days",
+                        color = DarkText,
+                        modifier = Modifier.padding(horizontal = 24.dp)
+                    )
+                }
+                LazyRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp, horizontal = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    items(7) { index ->
+                        DayButton(number = index.plus(1).toString()) {}
+                    }
+                }
                 AddButton(modifier = Modifier, onClick = { viewModel.createPlant() })
                 Spacer(modifier = Modifier.padding(bottom = 16.dp))
             }
