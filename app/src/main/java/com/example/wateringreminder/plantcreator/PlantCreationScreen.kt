ffea764ee32f8991258593df9e25f27bb7aa70e1
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-04-13 16:15:57 +0200

    Add plant name form

--- app/src/main/java/com/example/wateringreminder/plantcreator/PlantCreationScreen.kt
+++ app/src/main/java/com/example/wateringreminder/plantcreator/PlantCreationScreen.kt
@@ -1,37 +1,34 @@
 package com.example.wateringreminder.plantcreator
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
-import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import com.example.wateringreminder.LabelTextField
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun PlantCreationScreen() {
 
-    var viewModel: PlantCreationViewModel = koinViewModel()
-    var plantName by remember {
-        mutableStateOf("")
-    }
+    val viewModel: PlantCreationViewModel = koinViewModel()
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        TextField(
-            value = plantName,
-            onValueChange = { text ->
-                plantName = text
-            }
-        )
-        Button(onClick = { viewModel.createPlant(plantName) }) {
-
+        LabelTextField("Name", onValueChange = { viewModel.updatePlantName(it) })
+        Button(
+            onClick = { }
+        ) {
+            Text("Add")
         }
     }
 }
