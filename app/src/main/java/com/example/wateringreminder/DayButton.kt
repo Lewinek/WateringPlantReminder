38c587875a28d142dc8be0c9bcb7ef517c1e767b
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-04-14 11:10:57 +0200

    Add an option to select only one day button

--- app/src/main/java/com/example/wateringreminder/DayButton.kt
+++ app/src/main/java/com/example/wateringreminder/DayButton.kt
@@ -6,25 +6,33 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.wateringreminder.ui.theme.BackgroundGrey
+import com.example.wateringreminder.ui.theme.DarkText
 import com.example.wateringreminder.ui.theme.LightText
 
 @Composable
-fun DayButton(number: String, onClick: () -> Unit) {
+fun DayButton(dayNumber: Int, _selectedIndex: Int, onClick: (Int) -> Unit) {
+
+    var selectedIndex = _selectedIndex
+    val color = if (selectedIndex == dayNumber) DarkText else BackgroundGrey
+
     Button(
-        onClick = { onClick() },
-        modifier = Modifier.clip(RoundedCornerShape(8.dp)),
-        colors = ButtonDefaults.buttonColors(backgroundColor = BackgroundGrey)
+        onClick = {
+            selectedIndex = if (selectedIndex == dayNumber) -1 else dayNumber
+            onClick(selectedIndex)
+        },
+        Modifier.clip(RoundedCornerShape(8.dp)),
+        colors = ButtonDefaults.buttonColors(backgroundColor = color)
     ) {
         Text(
-            text = number,
+            text = dayNumber.toString(),
             modifier = Modifier
-                .background(color = BackgroundGrey)
+                .background(color = color)
                 .padding(vertical = 8.dp),
             color = LightText,
             fontSize = 18.sp
