c1159a4dad53a10525df0693bf8855e3e738b35f
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-04-20 11:39:08 +0200

    Move the shapes to a separate class

--- app/src/main/java/com/example/wateringreminder/ui/theme/Theme.kt
+++ app/src/main/java/com/example/wateringreminder/ui/theme/Theme.kt
@@ -1,12 +1,10 @@
 package com.example.wateringreminder.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
 
 private val AppDarkColorScheme = darkColorScheme(
     primary = Purple200,
@@ -45,9 +43,3 @@ fun WateringReminderTheme(
         content = content
     )
 }
-
-val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
-)
