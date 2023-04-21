c2e6ab2e15349deaaa5f5d7ea33498cd5d6e239d
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-04-21 20:08:42 +0200

    Add color and icon to FAB

--- app/src/main/java/com/example/wateringreminder/articlelist/MyPlantsScreen.kt
+++ app/src/main/java/com/example/wateringreminder/articlelist/MyPlantsScreen.kt
@@ -2,14 +2,20 @@ package com.example.wateringreminder.articlelist
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.wateringreminder.R
+import com.example.wateringreminder.ui.theme.DarkText
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,10 +25,14 @@ fun MyPlantsScreen(onNavigateToPlantCreator: () -> Unit) {
         floatingActionButton = {
             FloatingActionButton(
                 onClick = onNavigateToPlantCreator,
-                modifier = Modifier.padding(40.dp)
-            ) {}
-        },
-        containerColor = Color.White
+                modifier = Modifier
+                    .padding(40.dp)
+                    .clip(RoundedCornerShape(12.dp)),
+                containerColor = DarkText
+            ) {
+                Icon(painter = painterResource(id = R.drawable.add), contentDescription = "")
+            }
+        }, containerColor = Color.White
     ) {}
 }
 
