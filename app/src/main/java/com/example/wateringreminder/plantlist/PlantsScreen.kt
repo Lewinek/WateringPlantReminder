bd454fb8f3d4ce19cf6aab5a285577f31c1219cd
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-04-21 14:10:10 +0200

    Add water text at the top of the column

--- app/src/main/java/com/example/wateringreminder/plantlist/PlantsScreen.kt
+++ app/src/main/java/com/example/wateringreminder/plantlist/PlantsScreen.kt
@@ -9,9 +9,12 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.wateringreminder.PlantItem
 import com.example.wateringreminder.PlantsViewModel
+import com.example.wateringreminder.ui.theme.DarkText
 import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,14 +34,22 @@ fun PlantsScreen(
         },
         containerColor = Color.White
     ) {
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.padding(
-                top = 16.dp,
+        Column() {
+            Text(
+                text = "Water Today",
+                modifier = Modifier
+                    .padding(16.dp),
+                color = DarkText,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
             )
-        ) {
-            items(state.plants) { plant ->
-                PlantItem(plant)
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(bottom = 8.dp)
+            ) {
+                items(state.plants) { plant ->
+                    PlantItem(plant)
+                }
             }
         }
     }
