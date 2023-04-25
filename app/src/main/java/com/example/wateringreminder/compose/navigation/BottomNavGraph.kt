4f5be2a93c75fabc477d9def948953b0c2a05706
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-04-25 14:52:40 +0200

    Rename package, add column to MyPlants layout

--- app/src/main/java/com/example/wateringreminder/compose/navigation/BottomNavGraph.kt
+++ app/src/main/java/com/example/wateringreminder/compose/navigation/BottomNavGraph.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.wateringreminder.articlelist.MyPlantsScreen
+import com.example.wateringreminder.myplants.MyPlantsScreen
 import com.example.wateringreminder.watering.WateringScreen
 
 
--- app/src/main/java/com/example/wateringreminder/myplants/MyPlantsScreen.kt
+++ app/src/main/java/com/example/wateringreminder/myplants/MyPlantsScreen.kt
@@ -1,13 +1,13 @@
-package com.example.wateringreminder.articlelist
+package com.example.wateringreminder.myplants
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -35,7 +35,21 @@ fun MyPlantsScreen(onNavigateToPlantCreator: () -> Unit) {
             }
         }, containerColor = Color.White
     ) {
+        LazyColumn() {
+            items(
+                items = listOf(""),
+                key = {},
+                itemContent = { item ->
+                    val currentItem by rememberUpdatedState(item)
+                    val dismissState = rememberDismissState(
+                        confirmValueChange = {
+                           }
+                    )
+                }
+            )
+        }
         MyPlantItem()
+
     }
 }
 
