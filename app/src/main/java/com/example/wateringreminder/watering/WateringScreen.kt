4cf3ec87435f93e2b0f51ebbce08816ccc2dc69f
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-04-27 10:35:37 +0200

    Add fillMaxHeight to watering column

--- app/src/main/java/com/example/wateringreminder/watering/WateringScreen.kt
+++ app/src/main/java/com/example/wateringreminder/watering/WateringScreen.kt
@@ -1,10 +1,7 @@
 package com.example.wateringreminder.watering
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -34,7 +31,8 @@ fun WateringScreen() {
         )
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(8.dp),
-            contentPadding = PaddingValues(bottom = 8.dp)
+            contentPadding = PaddingValues(bottom = 8.dp),
+            modifier = Modifier.background(color = Color.White).fillMaxHeight()
         ) {
             items(state.plants) { plant ->
                 PlantItem(plant)
