e93f54cbcb61784692bc335485d020c231e685ff
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-05-17 22:05:26 +0200

    Remove unnecessary plant variables

--- data_source/src/main/java/com/example/data_source/Plant.kt
+++ data_source/src/main/java/com/example/data_source/Plant.kt
@@ -3,7 +3,5 @@ package com.example.data_source
 data class Plant(
     val id: Int? = null,
     val name: String,
-    val location: String? = null,
-    val wateringDate: String? = null,
-    val numberOfDaysToWatering: Int? = null
+    val location: String? = null
 )
