efd0b331a3d99cb8d319f9084af46804c01f1d67
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-05-24 11:45:09 +0200

    Add grouping before dates to today's date

--- app/src/main/java/com/example/wateringreminder/watering/WateringViewModel.kt
+++ app/src/main/java/com/example/wateringreminder/watering/WateringViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.data_source.EventRepository
 import com.example.data_source.local.PlantCached
-import com.example.wateringreminder.extensions.isToday
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 
@@ -23,15 +22,18 @@ class WateringViewModel(
     }
 
     private fun getPlantsThatNeedWatering() {
+        val today = LocalDate.now()
         viewModelScope.launch {
-            val plants = useCase().groupBy { it.date }
+            val plants = useCase().groupBy {
+                if (it.date.isBefore(today)) today else it.date
+            }
             _state.value = state.value.copy(plants = plants)
         }
     }
 
     fun changeWaterState(notification: WaterThePlantNotification) {
         viewModelScope.launch {
-            if (notification.date.isToday()){
+            if (!notification.date.isAfter(LocalDate.now())) {
                 val event = eventRepository.getEvents().findLast { notification.eventId == it.id }
                 val newEvent = event?.copy(isWatered = !event.isWatered)
                 newEvent?.let { eventRepository.updateEvent(it) }
