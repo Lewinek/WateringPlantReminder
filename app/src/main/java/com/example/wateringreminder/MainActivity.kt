5f89f4955755942f994419f622a8daa1964673d2
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-05-25 14:49:18 +0200

    Add a function that calls at midnight

--- app/src/main/java/com/example/wateringreminder/MainActivity.kt
+++ app/src/main/java/com/example/wateringreminder/MainActivity.kt
@@ -3,15 +3,39 @@ package com.example.wateringreminder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.wateringreminder.ui.theme.WateringReminderTheme
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        scheduleNewDayTask()
         setContent {
             WateringReminderTheme {
-               MainScreen()
+                MainScreen()
             }
         }
+    }
+
+    private fun scheduleNewDayTask() {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = System.currentTimeMillis()
+        calendar.set(Calendar.HOUR_OF_DAY, 14)
+        calendar.set(Calendar.MINUTE, 33)
+        calendar.set(Calendar.SECOND, 0)
+
+        val currentTime = calendar.timeInMillis
+        val timeDiff = currentTime - System.currentTimeMillis()
+
+        val showMsgWorkRequest = OneTimeWorkRequestBuilder<UpdateEventWorker>()
+            .setInitialDelay(timeDiff, TimeUnit.MILLISECONDS)
+            .build()
+
+        WorkManager.getInstance(this).enqueue(showMsgWorkRequest)
     }
 }
