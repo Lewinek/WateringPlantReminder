f96800c40a14db3798ef6079ecd4978349fe8444
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-05-28 19:35:13 +0200

    Remove scheduleNewDayTask()

--- app/src/main/java/com/example/wateringreminder/MainActivity.kt
+++ app/src/main/java/com/example/wateringreminder/MainActivity.kt
@@ -3,49 +3,18 @@ package com.example.wateringreminder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequest
-import androidx.work.WorkManager
 import com.example.wateringreminder.ui.theme.WateringReminderTheme
-import java.util.*
-import java.util.concurrent.TimeUnit
+import org.koin.android.ext.android.inject
 
-class MainActivity : ComponentActivity() {
+class MainActivity() : ComponentActivity() {
+    val scheduleNewDayTaskUseCase: ScheduleNewDayTaskUseCase by inject()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        scheduleNewDayTask()
+        scheduleNewDayTaskUseCase()
         setContent {
             WateringReminderTheme {
                 MainScreen()
             }
         }
-    }
-
-    private fun scheduleNewDayTask() {
-        val currentDate = Calendar.getInstance()
-        val dueDate = Calendar.getInstance()
-
-        dueDate.set(Calendar.HOUR_OF_DAY, 18)
-        dueDate.set(Calendar.MINUTE, 51)
-        dueDate.set(Calendar.SECOND, 0)
-        if (dueDate.before(currentDate)) {
-            dueDate.add(Calendar.HOUR_OF_DAY, 24)
-        }
-
-        val timeDiff = dueDate.timeInMillis - currentDate.timeInMillis
-        val minutes = TimeUnit.MILLISECONDS.toMinutes(timeDiff)
-
-        val refreshCpnWork =
-            PeriodicWorkRequest.Builder(UpdateEventWorker::class.java, 1, TimeUnit.DAYS)
-                .setInitialDelay(minutes, TimeUnit.MINUTES)
-                .addTag("myWorkManager")
-                .build()
-
-        WorkManager.getInstance(this)
-            .enqueueUniquePeriodicWork(
-                "myWorkManager",
-                ExistingPeriodicWorkPolicy.UPDATE,
-                refreshCpnWork
-            )
     }
 }
