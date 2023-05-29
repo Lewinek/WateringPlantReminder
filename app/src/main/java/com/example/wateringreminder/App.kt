3a758fd45346bcce7356fb6916c67c5e6f2da4f9
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-05-29 08:30:40 +0200

    Start scheduleNewDayTaskUseCase in App class

--- app/src/main/java/com/example/wateringreminder/App.kt
+++ app/src/main/java/com/example/wateringreminder/App.kt
@@ -2,15 +2,18 @@ package com.example.wateringreminder
 
 import android.app.Application
 import com.example.data_source.dataSourceModule
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.workmanager.koin.workManagerFactory
 import org.koin.core.context.startKoin
 
 class App : Application() {
+    val scheduleNewDayTaskUseCase: ScheduleNewDayTaskUseCase by inject()
 
     override fun onCreate() {
         super.onCreate()
         initKoin()
+        scheduleNewDayTaskUseCase()
     }
 
     private fun initKoin() {
--- app/src/main/java/com/example/wateringreminder/MainActivity.kt
+++ app/src/main/java/com/example/wateringreminder/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.wateringreminder.ui.theme.WateringReminderTheme
-import org.koin.android.ext.android.inject
 
-class MainActivity() : ComponentActivity() {
-    val scheduleNewDayTaskUseCase: ScheduleNewDayTaskUseCase by inject()
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        scheduleNewDayTaskUseCase()
         setContent {
             WateringReminderTheme {
                 MainScreen()
