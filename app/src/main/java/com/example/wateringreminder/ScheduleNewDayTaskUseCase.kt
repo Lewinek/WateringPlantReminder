447acf623e1bfe0306302a69abcc300b95ab045f
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-05-30 16:48:24 +0200

    Add abstraction layer to ScheduleNewDayTaskUseCase

--- app/src/main/java/com/example/wateringreminder/ScheduleNewDayTaskUseCase.kt
+++ app/src/main/java/com/example/wateringreminder/ScheduleNewDayTaskUseCase.kt
@@ -1,33 +1,45 @@
 package com.example.wateringreminder
 
 import android.content.Context
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
 import java.util.*
 import java.util.concurrent.TimeUnit
 
-class ScheduleNewDayTaskUseCase(private val context: Context, ) {
+class ScheduleNewDayTaskUseCase(private val workManager: WorkManager) : WorkEnqueue {
     operator fun invoke() {
-        val refreshCpnWork =
-            PeriodicWorkRequest.Builder(UpdateEventWorker::class.java, 1, TimeUnit.DAYS)
-                .setInitialDelay(calculateInitialDelay(), TimeUnit.MINUTES)
-                .addTag("myWorkManager")
-                .build()
+        val refreshCpnWork = PeriodicWorkRequest.Builder(
+            UpdateEventWorker::class.java, 1, TimeUnit.DAYS)
+            .setInitialDelay(calculateInitialDelay(), TimeUnit.MINUTES)
+            .addTag("myWorkManager")
+            .build()
 
-        WorkManager.getInstance(context).enqueue(refreshCpnWork)
+        enqueue(refreshCpnWork)
     }
 
-    private fun calculateInitialDelay(): Long {
-        val currentDate = Calendar.getInstance()
-        val dueDate = Calendar.getInstance()
-
-        dueDate.set(Calendar.HOUR_OF_DAY, 8)
-        dueDate.set(Calendar.MINUTE, 20)
-        dueDate.set(Calendar.SECOND, 0)
-        if (dueDate.before(currentDate)) {
-            dueDate.add(Calendar.HOUR_OF_DAY, 24)
+    fun calculateInitialDelay(): Long {
+        val dueDate = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 8)
+            set(Calendar.MINUTE, 20)
+            set(Calendar.SECOND, 0)
+            if (before(Calendar.getInstance())) {
+                add(Calendar.HOUR_OF_DAY, 24)
+            }
         }
 
-        return TimeUnit.MILLISECONDS.toMinutes(dueDate.timeInMillis - currentDate.timeInMillis)
+        return TimeUnit.MILLISECONDS.toMinutes(dueDate.timeInMillis - System.currentTimeMillis())
     }
+
+    override fun enqueue(workRequest: PeriodicWorkRequest) {
+        workManager.enqueueUniquePeriodicWork(
+            "myWorkManager",
+            ExistingPeriodicWorkPolicy.KEEP,
+            workRequest
+        )
+    }
+}
+
+interface WorkEnqueue {
+    fun enqueue(workRequest: PeriodicWorkRequest)
 }
