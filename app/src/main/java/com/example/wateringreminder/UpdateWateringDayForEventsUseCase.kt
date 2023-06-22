1bcb29651cf918768504d4304763a6960d965ef0
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-06-22 12:08:57 +0200

    Separate the update of watered events into a  function

--- app/src/main/java/com/example/wateringreminder/UpdateWateringDayForEventsUseCase.kt
+++ app/src/main/java/com/example/wateringreminder/UpdateWateringDayForEventsUseCase.kt
@@ -1,21 +1,25 @@
 package com.example.wateringreminder
 
 import com.example.data_source.EventRepository
+import com.example.data_source.local.Event
 import java.time.LocalDate
 
 class UpdateWateringDayForEventsUseCase(private val eventRepository: EventRepository) {
 
     suspend operator fun invoke() {
         val events = eventRepository.getEvents()
-        events.filter {
-            it.wateringDate.isBefore(LocalDate.now())
-            it.isWatered
+        val updatedEvents = updateOverdueWateringEvents(events)
+        eventRepository.updateEvents(updatedEvents)
+    }
+
+    fun updateOverdueWateringEvents(events: List<Event>): List<Event> {
+        return events.filter {
+            it.wateringDate.isBefore(LocalDate.now()) && it.isWatered
         }.map {
             it.copy(
                 wateringDate = LocalDate.now().plusDays(it.recurringInterval.toLong()),
                 isWatered = false
             )
         }
-        eventRepository.updateEvents(events)
     }
 }
