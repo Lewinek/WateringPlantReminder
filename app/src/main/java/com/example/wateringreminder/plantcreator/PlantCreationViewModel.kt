ba04f117ea2ffec162a0a674ab4d0d573b11f595
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-07-23 20:46:02 +0200

    Add event creation useCase to PlantCreationViewModel

--- app/src/main/java/com/example/wateringreminder/plantcreator/PlantCreationViewModel.kt
+++ app/src/main/java/com/example/wateringreminder/plantcreator/PlantCreationViewModel.kt
@@ -2,21 +2,15 @@ package com.example.wateringreminder.plantcreator
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.data_source.EventRepository
 import com.example.data_source.Plant
-import com.example.data_source.PlantRepository
-import com.example.data_source.local.Event
-import com.example.data_source.local.PlantCached
 import com.example.wateringreminder.utils.constants.Constants
 import com.example.wateringreminder.utils.validators.EmptyTextFieldValidator
 import com.example.wateringreminder.watering.WateringUiState
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import java.time.LocalDate
 
 class PlantCreationViewModel(
-    private val plantRepository: PlantRepository,
-    private val eventRepository: EventRepository
+    private val createPlantUseCase: CreatePlantUseCase
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(WateringUiState())
@@ -29,26 +23,13 @@ class PlantCreationViewModel(
     fun createPlant() {
         if (!_uiState.value.showNameError) {
             viewModelScope.launch {
-                val plantId = plantRepository.insertPlant(
-                    Plant(
-                        name = uiState.value.plantName,
-                        location = Constants.LOCATION
-                    )
-                )
-                val plant = plantRepository.getPlantById(plantId.toInt())
-                eventRepository.insertEvent(createEvent(plant))
+                createPlantUseCase(Plant(
+                    name = uiState.value.plantName,
+                    location = Constants.LOCATION
+                ))
                 _events.emit(UiEvents.FinishScreen)
             }
         }
-    }
-
-    private fun createEvent(plant: PlantCached): Event {
-        return Event(
-            wateringDate = LocalDate.now(),
-            recurringInterval = 1,
-            plantCached = plant,
-            isWatered = false
-        )
     }
 
     private fun validatePlantName(plantName: String) {
